e0e5e668bcf6f086892f20ba7af42d2bf15fa1ce
jitendra <jitendra.vaishnav@bhnetwork.com>
2020-10-08 23:43:16 +0530

    Reproduce nested transformation issue
    payload
    {
        "name": "nstry",
        "age": 5,
        "breed":"ok",
        "house": {
            "name": "wowo"
        }
    }
    house is transformed as an object not as a House

--- sample/01-cats-app/src/main.ts
+++ sample/01-cats-app/src/main.ts
@@ -1,12 +1,13 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {ValidationPipe} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({transform: true}));
 
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+    await app.listen(3000);
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
